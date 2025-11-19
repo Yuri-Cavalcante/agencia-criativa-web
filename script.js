ef4c4cb0f1c6a10d078d2f2bb0e8c263d5d17dca
Yuri Costa <yuric.cavalcante3@gmail.com>
2025-11-19 02:30:30 -0300

    att layout ts, att visual geral

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 20) {
+        header.classList.add('header-fosco');
+    } else {
+        header.classList.remove('header-fosco');
+    }
+})
+
+document.getElementById('frame-proposta').addEventListener('mouseover', ()=> {
+    console.log('aaa')
+})
