3fd078bf3dfca4e99474ee8616659da2d7c6c861
Yuri Costa <yuric.cavalcante3@gmail.com>
2025-11-23 00:12:12 -0300

    refatoracao BEM

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     if (window.scrollY > 20) {
-        header.classList.add('header-fosco');
+        header.classList.add('header--fosco');
     } else {
-        header.classList.remove('header-fosco');
+        header.classList.remove('header--fosco');
     }
 })
